e8403a034c8c8f5d1d803643a6f907f550fa0d18
Luigi Masini <luigimasini.lm@gmail.com>
2021-10-13 01:17:35 +0200

    added todos & setImageDestination & enhanced a little the flow

--- src/bot.js
+++ src/bot.js
@@ -12,6 +12,10 @@ const bot = new Telegraf(process.env.telegramBotToken)
 //look	https://blog.logrocket.com/how-to-build-a-telegram-ocr-bot/
 //and	https://telegraf.js.org/classes/MemorySessionStore.html
 
+//TODO accorpare codice duplicato in funzioni
+
+//TODO dare nomi meaningful a funzioni
+
 function authorizeInNotion (ctx, again=false){
 	
 	const notionAuthorizationUrl = new URL ("https://api.notion.com/v1/oauth/authorize")
@@ -72,10 +76,74 @@ bot.start(ctx=>{
 
 bot.command('cancel', ctx=>cache.del(ctx.chat.id.toString()))
 
+/*TODO
+instead of 
+ - message 1: Selected template : n
+ - message 2: Changing Page
+ - message 3: Selected Workspace : n
+ etc 
+ 
+ have message 1 with all that information, store its message_id in cache & modify it every time, add new information & needed buttons
+ */
 //TODO disposizioni e paginazione di pulsanti
 //TODO cancellare cache a fine operazioni
+//TODO add catch & error handling in every function
+//### maybe add back button here and there in keyboards
 //NOTE in cache.set all expansions in 'value' argument should go first, or they will overwrite changes, resulting in no change at all
 
+
+
+bot.action(/setImageDestination(\d+)/i, ctx=>
+		ctx.answerCbQuery()
+		.then(() => ctx.editMessageText(ctx.callbackQuery.message.text))
+		.then(() => {
+			const data = cache.get(ctx.chat.id.toString())
+			
+			if (!data)
+				return ctx.reply("We lost your cached data, please start the operation again.\n\nSorry for the incovenience")
+			
+			return db.promise('UPDATE `Templates` SET `ImageDestination`=? WHERE id=?', [ctx.match[1], data.templateData.id])
+		})
+		.then(({error}) => {
+			if (!!error)
+				throw new Error(error.code+" - "+error.sqlMessage)
+			
+			return ctx.reply("Done: now template "+data.templateData.userTemplateNumber+" is saving images to "+["content", "cover", "icon"][ctx.match[1]])
+		})
+		//WARNING tmp, if not op:edit will switch to adding text rules instead of getting back to selectTemplate
+		.then(() => {
+			
+			const data = cache.get(ctx.chat.id.toString())
+			
+			if (!data)
+				return ctx.reply("We lost your cached data, please start the operation again.\n\nSorry for the incovenience")
+			
+			cache.set(ctx.chat.id.toString(), {...data, op:'edit'})
+			
+			return selectTemplate(data.templateData.id, ctx)
+		})
+		.catch(err => {
+			console.warn(err)
+			ctx.reply("Error updating image destination: "+err+"\n\nYou can try again later, search the error or report the incident.")
+		})
+	  )
+
+
+const editImageDestination = ctx => ctx.reply(
+	"In which section of the page do you want me to save images you send me?",
+	Markup.inlineKeyboard([
+		Markup.button.callback("Content", 'setImageDestination0'),
+		Markup.button.callback("Cover", 'setImageDestination1'),
+		Markup.button.callback("Icon", 'setImageDestination2')
+		//TODO add cancel button
+	])
+)
+
+bot.action('editImageDestination', ctx =>
+		ctx.answerCbQuery()
+		.then(() => ctx.editMessageText(ctx.callbackQuery.message.text))
+		.then(() => editImageDestination(ctx))
+	  )
 
 const selectPage = (pageId, ctx) => {
 	
@@ -84,14 +152,24 @@ const selectPage = (pageId, ctx) => {
 	if (!data)
 		return ctx.reply("We lost your cached data, please start the operation again.\n\nSorry for the incovenience")
 	
-	const {pages, ...userData} = data
+	const {pages, templateData, workspaceData, op, ...userData} = data
 	
 	const page = pages.filter( ({id}) => id === pageId)[0]
 	
-	ctx.editMessageText("Selected page: "+page.title)
-	
-	return 
-	
+	//can always call editMessageText because selectPage is always called from bot.action
+	return ctx.editMessageText("Selected page: "+page.title)
+	.then(() => db.promise('UPDATE `Templates` SET pageId=? WHERE id=?', [page.id, templateData.id]))
+	.then(({error}) => {
+		
+		if(!!error)
+			throw new Error(error.code+" - "+error.sqlMessage)
+		
+		if (!!op && op === 'edit')
+			return ctx.reply("All good: page for template "+templateData.userTemplateNumber+" set to "+page.title+" in workspace named "+workspaceData.name)
+			.then(()=>selectTemplate(templateData.id, ctx))
+		
+		return editImageDestination(ctx)
+	})	
 }
 
 bot.action(/selectPG(\d+)/i, ctx=>
@@ -120,6 +198,7 @@ bot.action(/registerPG(\d+)/i, ctx=>{
 	.then(()=>db.promise('SELECT MAX(id) as id, pageType, icon, title FROM NotionPages WHERE chatId=?', [data.templateData.chatId]))
 	.then( ({result}) => result[0].pageType === "pg" ?
 		({state:result}) :
+		//cannot use db.promise ( === db.execute) cuz cannot bulk import with prepared statements, using db.promiseQuery ( === db.query )
 		db.promiseQuery(
 			   'INSERT INTO `NotionPagesProps` (`notionPropId`, `pageId`, `propName`, `propTypeId`) VALUES ?',
 			   [Object.entries(properties).map( ([key, value]) => ([value.id, result[0].id, value.name, notion.propTypes.indexOf(value.type)]))],
@@ -220,7 +299,7 @@ const selectWorkspace = (workspaceId, ctx) => {
 				"Select a page:",
 				Markup.inlineKeyboard([
 					...result.map(({id, title}, key)=>Markup.button.callback((key+1)+" - "+title, "selectPG"+id)),
-					Markup.button.callback('+', 'addPage')
+					Markup.button.callback('+', 'addPage'),
 				])
 			)
 		
@@ -241,6 +320,62 @@ bot.action('addWorkspace', ctx=>
 		.then(()=>authorizeInNotion(ctx))
 	  )
 
+const preSelectWorkspace = ctx => {
+	
+	const data = cache.get(ctx.chat.id.toString())
+	
+	if (!data)
+		return ctx.reply("We lost your cached data, please start the operation again.\n\nSorry for the incovenience")
+	
+	const {templateData, ...userData} = data
+	
+	//check if user has any workspace
+	return db.promise('SELECT wc.accessToken, wc.workspaceId, nw.name, nw.icon FROM NotionWorkspacesCredentials as wc LEFT OUTER JOIN NotionWorkspaces as nw ON nw.id=wc.workspaceId WHERE wc.chatId=?', [templateData.chatId])
+	.then(({result})=>{
+		
+		//keep just the selected template & save workspaces
+		cache.set(ctx.chat.id.toString(), {...data, workspaces:result})
+		
+		//if no workspaces authorized
+		if (!result || !result.length)
+			return ctx.reply("No workspace found. Add one then try again:")
+			//TODO see preceding TODO
+			.then(()=>authorizeInNotion(ctx))
+			
+		//select a workspace
+		else
+			//TODO display nw.icon if emoji in button text
+			return ctx.reply(
+				"Select a workspace:",
+				Markup.inlineKeyboard([
+					...result.map(({workspaceId, name}, key)=>Markup.button.callback((key+1)+" - "+name, "selectWK"+workspaceId)),
+					Markup.button.callback('+', 'addWorkspace'),
+					//TODO add cancel button
+				])
+			)
+	})
+}
+
+bot.action('changePage',  ctx => 
+		ctx.answerCbQuery()
+		.then(()=>ctx.editMessageText("Changing template's page"))
+		.then(()=>preSelectWorkspace(ctx))
+	  )
+
+bot.action('changeRule', ctx =>
+		ctx.answerCbQuery()
+		.then(()=>ctx.editMessageText("Editing rules"))
+		.then(()=>ctx.reply(
+			"Do you whant to edit what to do with image or text?",
+			Markup.inlineKeyboard([
+				Markup.button.callback('Image', 'editImageDestination'),
+				Markup.button.callback('Text', 'editTextRules'),
+				//NOTE will add audio & files
+				//TODO add cancel button
+			])
+		))
+	  )
+
 const selectTemplate = (templateId, ctx) => {
 	
 	const data = cache.get(ctx.chat.id.toString())
@@ -249,40 +384,45 @@ const selectTemplate = (templateId, ctx) => {
 		return ctx.reply("We lost your cached data, please start the operation again.\n\nSorry for the incovenience")
 	
 	//get array of templates
-	const templateData = data.templates.filter(({id})=>id===templateId)[0]
+	const {templates, ...userData} = data
 	
-	ctx.callbackQuery && ctx.callbackQuery.message.from.username === 'NotionSaverBot' && ctx.editMessageText("Selected Template : "+templateData.userTemplateNumber)
+	if (!!templates){
+		const templateData = templates.filter(({id})=>id===templateId)[0]
+		
+		ctx.callbackQuery && ctx.callbackQuery.message.from.username === 'NotionSaverBot' && ctx.editMessageText("Selected Template : "+templateData.userTemplateNumber)
+		
+		cache.set(ctx.chat.id.toString(), {...userData, templateData})
+		
+		//if the selected template has NULL pageId automatically add it
+		if (!templateData.pageId)
+			return preSelectWorkspace(ctx)
+	}
 	
-	//check if user has any workspace
-	return db.promise('SELECT wc.accessToken, wc.workspaceId, nw.name, nw.icon FROM NotionWorkspacesCredentials as wc LEFT OUTER JOIN NotionWorkspaces as nw ON nw.id=wc.workspaceId WHERE wc.chatId=?', [templateData.chatId])
-	.then(({result})=>{
-		
-		//keep just the selected template & save workspaces
-		cache.set(ctx.chat.id.toString(), {templateData, workspaces:result})
-		
-		//if no workspaces authorized
-		if (!result || !result.length)
-			return ctx.reply("No workspace found. Add one then try again:")
-			//TODO see preceding TODO
-			.then(()=>authorizeInNotion(ctx))
-		
-		//select a workspace
-		else
-			//TODO display nw.icon if emoji in button text
-			return ctx.reply(
-				"Select a workspace:",
-				Markup.inlineKeyboard([
-					...result.map(({workspaceId, name}, key)=>Markup.button.callback((key+1)+" - "+name, "selectWK"+workspaceId)),
-					Markup.button.callback('+', 'addWorkspace')
-				])
-			)
-	})
+	if (!!userData.op && userData.op === "edit")
+		return ctx.reply(
+			//TODO insert here current template configuration"
+			"What do you want to do?",
+			Markup.inlineKeyboard([
+				Markup.button.callback("Change page", 'changePage'),
+				Markup.button.callback("Change rules", 'changeRule'),
+				//TODO cancel button
+			]))
+	
+	return preSelectWorkspace(ctx)
 }
 
-bot.action(/selectTP(\d+)/i, ctx=>
-		ctx.answerCbQuery()
-		.then(()=>selectTemplate(parseInt(ctx.match[1]), ctx))
-	  )
+bot.action(/selectTP(\d+)/i, ctx=>{
+	
+	const data = cache.get(ctx.chat.id.toString())
+	
+	if (!data)
+		return ctx.reply("We lost your cached data, please start the operation again.\n\nSorry for the incovenience")
+	
+	cache.set(ctx.chat.id.toString(), {...data, op:'edit'})
+	
+	return ctx.answerCbQuery()
+	.then(()=>selectTemplate(parseInt(ctx.match[1]), ctx))
+})
 
 const addTemplate = ctx =>{
 	
@@ -317,7 +457,7 @@ bot.command('configtemplates', ctx=>
 	//get user templates
 // 	db.promise('SELECT t.id, t.userTemplateNumber, p.pageType, p.icon, tc.id as chatId FROM Templates as t LEFT OUTER JOIN NotionPages as p ON p.id=t.pageId LEFT OUTER JOIN TelegramChats as tc ON tc.id = p.chatId WHERE tc.telegramChatId=? ORDER BY t.userTemplateNumber',[ctx.chat.id])
 	//WARNING tmp to not create a new template every time the operation will not  conclude
-	db.promise('SELECT t.id, t.userTemplateNumber, tc.id as chatId FROM Templates as t LEFT OUTER JOIN TelegramChats as tc ON tc.id = t.chatId WHERE tc.telegramChatId=? ORDER BY t.userTemplateNumber',[ctx.chat.id])
+	db.promise('SELECT t.id, t.userTemplateNumber, tc.id as chatId, t.pageId FROM Templates as t LEFT OUTER JOIN TelegramChats as tc ON tc.id = t.chatId WHERE tc.telegramChatId=? ORDER BY t.userTemplateNumber',[ctx.chat.id])
 	.then(({result})=>{
 		
 		
@@ -338,7 +478,8 @@ bot.command('configtemplates', ctx=>
 				"Select a template to edit",
 				Markup.inlineKeyboard([
 					...result.map(({id, userTemplateNumber}, key)=>Markup.button.callback(userTemplateNumber, "selectTP"+id)),
-					Markup.button.callback('+', 'addTemplate')
+					Markup.button.callback('+', 'addTemplate'),
+					//TODO add cancel button
 				])
 			)
 	})
